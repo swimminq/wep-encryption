792d5aed1213f6913b45d2d685d66829484bb095
swimminq <pcy4101@gs.cwnu.ac.kr>
2022-03-18 23:40:01 +0900

    Add function

--- RC4.py
+++ RC4.py
@@ -3,6 +3,7 @@ S = [x for x in range(0,256)]
 K = []
 key = "RC4"
 text = "Swimming"
+KeyStream = []
 
 # 초기화
 def init():
@@ -18,7 +19,36 @@ def shuffle():
         j = (j + S[i]) % 256
         S[i],S[j] = S[j],S[i]
 
+
     print(S)
+
+# 암호문과 복호문의 대칭키(KeyStream) 생성
+def CreateKey():
+    m = 0
+    n = 0
+
+    for _ in range(0, len(text)):
+        m = (m + 1) % 256
+        n = (n + S[m]) % 256
+        S[m], S[n] = S[n],S[m]
+
+        t = (S[m] + S[n]) % 256
+
+        KeyStream.append(S[t])
+    
+    print(KeyStream)
+
+
+def Encryption():
+    count = 0
+    crypto = []
+
+    for k in range(0, len(text)):
+        crypto.append(ord(text[k]) ^ KeyStream[k])
+
+    print(crypto)
 
 init()
 shuffle()
+CreateKey()
+Encryption()
